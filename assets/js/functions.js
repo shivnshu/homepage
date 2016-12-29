bac4ce93ba19be3c7006ee3cc9be2f45075c0702
shivnshu <shivnshus@gmail.com>
2016-12-29 16:53:11 +0530

    scrollToTop  working

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,24 +1,16 @@
-// var numbers = [12, 54, 32, 45, 21, 69, 20];
-// function generateNumber(index) {
-//   var desired = numbers[index];
-//   var duration = 2000;
-//
-//   var output = $('#output' + index); // Start ID with letter
-//   var started = new Date().getTime();
-//
-//   animationTimer = setInterval(function() {
-//     if (output.text().trim() === desired || new Date().getTime() - started > duration) {
-//       clearInterval(animationTimer); // Stop the loop
-//       output.text(desired); // Print desired number in case it stopped at a different one due to duration expiration
-//       generateNumber(index + 1);
-//     } else {
-//       output.text(
-//         '' +
-//         Math.floor(Math.random() * 10) +
-//         Math.floor(Math.random() * 10)
-//       );
-//     }
-//   }, 100);
-// }
-// 
-// generateNumber(0);
+$(document).ready(function (){
+
+    $(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.button-scroll-top').fadeIn();
+		} else {
+			$('.button-scroll-top').fadeOut();
+		}
+	});
+
+    $('#scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		return false;
+	});
+
+});
